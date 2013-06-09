4777669fd8c44b171467188214b01d932c1e0fc2
Bryan Bell <bryan.w.bell@gmail.com>
2013-06-09 07:36:38 -0700

    cleanup for jslint

--- closest-pair/timing-code.js
+++ closest-pair/timing-code.js
@@ -1,56 +1,64 @@
-function generatePoints(numPoints, x_range, y_range, minDist)
-{
-    var points = new Array();
-    for (var i = 0; i < numPoints; i++)
-    {
-	var dup = true;
-	while (dup)
-	{
-	    dup = false;
-	    var x = Math.floor(Math.random() * x_range);
-	    var y = Math.floor(Math.random() * y_range);
-	    for (var j = 0; j < i; j++){
-		if ((points[j][0] == x && points[j][1] == y) || dist(points[j], [x, y]) < minDist)
-		{
-		    dup = true;
-		    break;
-		}
-	    
-	    }
-	}
-	points.push([x, y]);	
+"use strict";
+var dist, computeClosestRecursive, clClosestPair, output;
+function generatePoints(numPoints, x_range, y_range, minDist) {
+    var points = [],
+        i,
+        j,
+        dup,
+        x,
+        y;
+
+    for (i = 0; i < numPoints; i = i + 1) {
+        dup = true;
+        while (dup) {
+            dup = false;
+            x = Math.floor(Math.random() * x_range);
+            y = Math.floor(Math.random() * y_range);
+            for (j = 0; j < i; j = j + 1) {
+                if ((points[j][0] === x && points[j][1] === y) || dist(points[j], [x, y]) < minDist) {
+                    dup = true;
+                    break;
+                }
+            }
+        }
+        points.push([x, y]);
     }
     return points;
 }
 
-function TimeOCL_JS()
-{
-    var points = new Array();
-    //var numPoints = 10;
-    for (var numPoints = 500; numPoints <= 3000; numPoints = numPoints + 500)
-    {
-	points = generatePoints(numPoints, 256 * numPoints, 256 * numPoints, Math.floor(Math.random() * 10));
-        points.sort(function (p1, p2) {return p1[0] - p2[0];});
-	var JS_Start = (new Date()).getTime();
-	var closestPoints = computeClosestRecursive(points);
-	var JS_Time = (new Date()).getTime() - JS_Start;
-	var OCL = clClosestPair(points, false, true, false);
-	var OCL_Time = OCL[0];
-	var output = document.getElementById("output");
+function timeOCLJS() {
+    var points = [],
+        numPoints,
+        JS_Start,
+        closestPoints,
+        JS_Time,
+        OCL,
+        OCL_Time,
+        sortFunction,
+        i;
+    sortFunction = function (p1, p2) {return p1[0] - p2[0]; };
+    for (numPoints = 500; numPoints <= 3000; numPoints = numPoints + 500) {
+        points = generatePoints(numPoints, 256 * numPoints, 256 * numPoints, Math.floor(Math.random() * 10));
+        points.sort(sortFunction);
+        JS_Start = (new Date()).getTime();
+        closestPoints = computeClosestRecursive(points);
+        JS_Time = (new Date()).getTime() - JS_Start;
+        OCL = clClosestPair(points, false, true, false);
+        OCL_Time = OCL[0];
+        output = document.getElementById("output");
         output.innerHTML += "<br/>";
         output.innerHTML += "NumPoints:" + numPoints + "<br/>";
-	    output.innerHTML += "JS: Time = " + JS_Time + " millisec(s)" + ", Pair: (" + points[closestPoints[0]][0] + ", " + points[closestPoints[0]][1] + "), (" + points[closestPoints[1]][0] + ", " + points[closestPoints[1]][1] + "), d = " + dist(points[closestPoints[0]], points[closestPoints[1]]);
+        output.innerHTML += "JS: Time = " + JS_Time + " millisec(s)" + ", Pair: (" + points[closestPoints[0]][0] + ", " + points[closestPoints[0]][1] + "), (" + points[closestPoints[1]][0] + ", " + points[closestPoints[1]][1] + "), d = " + dist(points[closestPoints[0]], points[closestPoints[1]]);
         output.innerHTML += "<br/>";
-	    output.innerHTML += "CL: Time = " + (OCL_Time) + " millisec(s)"  + ", Pair: (" + OCL[1] + ", " + OCL[2] + "), (" + OCL[3] + ", " + OCL[4] + "), d = " + dist([OCL[1], OCL[2]], [OCL[3], OCL[4]]);
-	    output.innerHTML += "<br/>";
+        output.innerHTML += "CL: Time = " + (OCL_Time) + " millisec(s)"  + ", Pair: (" + OCL[1] + ", " + OCL[2] + "), (" + OCL[3] + ", " + OCL[4] + "), d = " + dist([OCL[1], OCL[2]], [OCL[3], OCL[4]]);
+        output.innerHTML += "<br/>";
         if (dist(points[closestPoints[0]], points[closestPoints[1]]) !== dist([OCL[1], OCL[2]], [OCL[3], OCL[4]])) {
-            	output.innerHTML += "<br/>";
+            output.innerHTML += "<br/>";
             output.innerHTML += "Points: ";
-	    for (var i = 0; i < points.length; i++)
-            {
-	        output.innerHTML += "(" + points[i][0] + ", " + points[i][1] + "),";
+            for (i = 0; i < points.length; i = i + 1) {
+                output.innerHTML += "(" + points[i][0] + ", " + points[i][1] + "),";
             }
             break;
         }
-    }    
+    }
 }
